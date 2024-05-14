8830d3ce0b63630f159388293519d8636a877680
Sanni-Damilola <sannifortune11@gmail.com>
2024-05-14 15:23:19 +0100

    feat(repo): initialize and push specific folder to GitHub
    - Initialize a new Git repository in the specific folder
    - Add files to the repository
    - Commit initial changes
    - Create a new repository on GitHub
    - Link local repository to GitHub repository
    - Push only the main branch to GitHub

--- MiddleWare/AsyncHandler.ts
+++
@@ -1,5 +0,0 @@
-import { Response, Request, NextFunction } from "express";
-
-export const AsyncHandler =
-  (fn: any) => (req: Request, res: Response, next: NextFunction) =>
-    Promise.resolve(fn(req, res, next)).catch(next);
